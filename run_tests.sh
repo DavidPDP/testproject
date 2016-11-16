4d8c876313b27c301983bbf75cdaa3e8f389dbe6
DavidPDP <ballesto_@hotmail.com>
2016-11-16 08:20:30 -0500

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e 
 
-. ~/.virtualenvs/testproject/bin/activate
+. /var/lib/jenkins/.virtualenvs/testproject/bin/activate
 
 PYTHONPATH=. py.test --junitxml=python_tests.xml
